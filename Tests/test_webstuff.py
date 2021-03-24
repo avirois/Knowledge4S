f1b4d57d5aaeb8455c59f5b0e6859abc65812d77
OlegBelochitsky <olegbelo8@gmail.com>
2021-03-24 10:50:40 +0200

    added type anotations

--- Tests/test_webstuff.py
+++ Tests/test_webstuff.py
@@ -1,13 +1,14 @@
 import pytest
+from selenium import webdriver
 
 class TestWebStuff:
-    def test_title(self, app_init, driver_init):
+    def test_title(self, app_init: str, driver_init: webdriver.Firefox):
         url = app_init
         driver_init.get(url)
         assert "hello" == driver_init.title
 
  
-    def test_h1(self, app_init, driver_init):
+    def test_h1(self, app_init: str, driver_init: webdriver.Firefox):
         url = app_init
         driver_init.get(url)
         tmp = driver_init.find_element_by_tag_name('h1')
