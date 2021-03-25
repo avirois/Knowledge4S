b5da2d47556f4e09a65cc55ccfbdf36e1314b759
zfriza <zfriza20@gmail.com>
2021-03-25 11:49:16 +0200

    files uploaded now saves in storage folder with a restriction for some file types also a upload now button was modified

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, request
 from blueprints.index import index_blueprint
-from blueprints.uploads import upload_blueprint
-from werkzeug.utils import secure_filename
+from blueprints.uploads import upload_blueprint,UPLOAD_FOLDER
+
 
 app = Flask(__name__)
 
 app.register_blueprint(index_blueprint)
 app.register_blueprint(upload_blueprint)
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- blueprints/uploads.py
+++ blueprints/uploads.py
@@ -1,17 +1,26 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template,request
+from werkzeug.utils import secure_filename
+import os
 
 upload_blueprint = Blueprint("upload_blueprint", __name__, template_folder="templates")
+UPLOAD_FOLDER ='storage/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@upload_blueprint.route("/upload")
+@upload_blueprint.route("/upload",methods = ['GET', 'POST'])
 def upload():
-    return render_template("upload.html")
-
-	
-# @upload_blueprint.route('/uploader', methods = ['GET', 'POST'])
-# def upload_file():
-#    if request.method == 'POST':
-#       f = request.files['file']
-#       f.save(secure_filename(f.filename))
-#       return 'file uploaded successfully'
+    if request.method == 'POST':
+        # check if the post request has the file part
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            ##TODO add file info to db (course,school,fucilty,data,time,uploadbywhom)
+            return render_template("upload.html",data = { "msg" : "file uploaded successfully"})
+        else:
+            return render_template("upload.html",data = { "msg" : "file type is not supported."})
+    else:
+        return render_template("upload.html", data = {"msg" : ""})
