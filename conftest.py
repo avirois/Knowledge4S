6efbc65d52da9931371e0248a082194de94b85cd
OlegBelochitsky <olegbelo8@gmail.com>
2021-03-25 11:49:26 +0200

    rename variables & better way to check if server is up

--- conftest.py
+++ conftest.py
@@ -8,10 +8,10 @@ current fixtures:
     - driver_init :
 """
 import os
-import logging
-from io import StringIO
 import threading
 from time import sleep
+import urllib.request
+import urllib.error
 import pytest
 from selenium import webdriver
 from app import app
@@ -29,38 +29,35 @@ def thread_flask_app():
     app.run(host=HOST, port=PORT, debug=True, use_reloader=False)
 
 
-def is_server_running(timeout, output):
-    """If 'Running' been logged than server is running."""
-    time = 0
+def is_server_running(timeout: float, url: str):
+    """Try to open url untill timeout."""
+    time: float = 0
     while time < timeout:
-        if "Running" in output.getvalue():
-            return True
+        try:
+            if urllib.request.urlopen(url).getcode() == 200:
+                return True
+        except urllib.error.URLError:
+            pass  # TODO log error
         time += 0.1  # check interval
         sleep(0.1)
     return False
 
 
 @pytest.fixture(scope="session")
-def app_init():
+def application():
     """Fixture that start a server in deamon thread."""
     # adding custome logger to read from when server is started
-    logger = logging.getLogger("werkzeug")
-    output = StringIO()
-    handler = logging.StreamHandler(output)
-    logger.addHandler(handler)
     web_app = threading.Thread(name="Web App", target=thread_flask_app)
     web_app.setDaemon(True)
     web_app.start()
-    if not is_server_running(TIMEOUT, output):
-        logger.removeHandler(handler)
-        raise RuntimeError("time out %d seconds, server not up")
-    logger.removeHandler(handler)
     url = "http://" + HOST + ":" + str(PORT)
+    if not is_server_running(TIMEOUT, url):
+        raise RuntimeError("time out %d seconds, server not up" % TIMEOUT)
     yield url
 
 
 @pytest.fixture(scope="session")
-def driver_init():
+def ff_browser():
     """Fixture that starts firefox webdriver headlessly."""
     fire_fox_options = webdriver.FirefoxOptions()
     fire_fox_options.headless = True
