1d3afd51a6b6600cac22e5639f045de163dd0559
avielro <avirois@gmail.com>
2021-03-26 14:42:46 +0300

    BSPM2021T3-96 Created unit tests for login

--- Tests/test_register_login.py
+++ Tests/test_register_login.py
@@ -86,4 +86,17 @@ class TestRegister:
         facultyID = ""
         usr = User("testing", "aviel", "rois", "Aa123456!", 1, facultyID, 1)
         assert usr.validateUser() != ""
+
+class TestLogin:
+    def test_password_login_valid(self):
+        username = "t1"
+        password = "Aa123456!"
+        usr = User(username, "aaa", "aaa", "Aa123456!", 1, 1, 1)
+        assert usr.validatePassword(password)
+
+    def test_password_login_invalid(self):
+        username = "t1"
+        password = "Aa123"
+        usr = User(username, "aaa", "aaa", "Aa123456!", 1, 1, 1)
+        assert not usr.validatePassword(password)
     
