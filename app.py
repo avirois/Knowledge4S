1fc53b3d00c8e7426ff47e04f12507f51bf6cd0d
zfriza <zfriza20@gmail.com>
2021-03-27 15:00:03 +0300

    prepare for pull

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 from blueprints.index import index_blueprint
 from blueprints.uploads import upload_blueprint,UPLOAD_FOLDER
-
+import sqlite3,base64,os,datetime
 
 app = Flask(__name__)
 
 app.register_blueprint(index_blueprint)
 app.register_blueprint(upload_blueprint)
+app.register_blueprint(authentication_blueprint)
+
+# Configure Upload Folder
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Database name
+app.config['DB_NAME'] = 'database.db'
+
+# Configure secret key inorder to allow login
+app.secret_key = b'\xf5\xe4\xabr\x89\xd9#^D@0\xae[R1\xcf'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
