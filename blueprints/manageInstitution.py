740724500feb87d8686b46acadd057c9f119ae5e
avielro <avirois@gmail.com>
2021-03-30 17:09:13 +0300

    BSPM2021T3-117 Created backend functionality to create and edit institution

--- blueprints/manageInstitution.py
+++ blueprints/manageInstitution.py
@@ -26,6 +26,117 @@ def getInstitutions():
 
     return (institutions)
 
+@inst_manage_blueprint.route('/create_institution', methods=['POST', 'GET'])
+def create_institution():
+    # If method post selected then need to create the institution
+    if (request.method == "POST"):
+        # connect to db and check if institution already exists
+        con = sqlite3.connect(current_app.config['DB_NAME'])
+
+        # Get next institution ID
+        sqlQueryGetInstID = "SELECT seq FROM sqlite_sequence WHERE name = 'Institutions'"
+        sqlResID = con.execute(sqlQueryGetInstID)
+        record = sqlResID.fetchone()
+        
+        # Save the ID of the next institution
+        instID = int(record[0]) + 1
+
+        # Get the institution if already exists
+        sqlQueryCheckExist = "SELECT * FROM Institutions WHERE InstitutionName = (?)"
+        sqlRes = con.execute(sqlQueryCheckExist, (request.form["instName"],))
+        record = sqlRes.fetchone()
+
+        # Close the database connection
+        con.close()
+
+        # Create institution object
+        newInst = Institution(instID, request.form["instName"])
+        
+        # Check if the institution is not already exists
+        if (record == None):
+            # connect to DB
+            con = sqlite3.connect(current_app.config['DB_NAME'])
+
+            # Insert the institution into institutions table
+            sqlQueryCreateInst = "INSERT INTO Institutions (InstitutionName) VALUES (?)"
+            con.execute(sqlQueryCreateInst, (newInst.getName(),))
+
+            # Commit the changes in institutions table
+            con.commit()
+
+            # Close the database connection
+            con.close()
+            
+            return redirect('/manage_institutions')
+        else:
+            massage = "Institution already exists"
+            return render_template('add_edit_instit.html', operation = "Add new", massage = massage)
+
+    # Load and prepare the page
+    else:
+        return render_template('add_edit_instit.html', operation = "Add new", massage = "Please enter institution name")
+
+
+@inst_manage_blueprint.route('/edit_institution/<inst_ID>', methods=['POST', 'GET'])
+def edit_institution(inst_ID):
+
+    # If method post selected then need to create the institution
+    if (request.method == "POST"):
+        # connect to db and check if institution already exists
+        con = sqlite3.connect(current_app.config['DB_NAME'])
+
+        # Save the ID of the next institution
+        instID = int(inst_ID)
+
+        # Create institution object
+        newInst = Institution(instID, request.form["instName"])
+
+        # Get the institution data
+        sqlQueryGetData = "SELECT * FROM Institutions WHERE InstitutionName = (?)"
+        sqlRes = con.execute(sqlQueryGetData, (newInst.getName(),))
+        record = sqlRes.fetchone()
+
+        # Check if the institution is not already exists
+        if (record == None):
+            # connect to DB
+            con = sqlite3.connect(current_app.config['DB_NAME'])
+
+            # Update the institution name
+            sqlQueryUpdateInst = "UPDATE Institutions SET InstitutionName = (?) WHERE InstitutionID = (?)"
+            con.execute(sqlQueryUpdateInst, (newInst.getName(), newInst.getID()))
+
+            # Commit the changes in users table
+            con.commit()
+
+            # Close the database connection
+            con.close()
+            
+            return redirect('/manage_institutions')
+        else:
+            massage = "Institution already exists"
+            return render_template('add_edit_instit.html', operation = "Edit", massage = massage, institution = newInst.getName())
+
+        # Close the database connection
+        con.close()
+
+    # Load and prepare the page
+    else:
+        # connect to db and check if institution already exists
+        con = sqlite3.connect(current_app.config['DB_NAME'])
+
+        # Save the ID of the next institution
+        instID = int(inst_ID)
+
+        # Get the institution data
+        sqlQueryGetData = "SELECT * FROM Institutions WHERE InstitutionID = (?)"
+        sqlRes = con.execute(sqlQueryGetData, (instID,))
+        record = sqlRes.fetchone()
+
+        # Create institution object
+        newInst = Institution(instID, record[1])
+
+        return render_template('add_edit_instit.html', operation = "Edit", massage = "Please enter institution name", institution = newInst.getName())
+
 @inst_manage_blueprint.route('/manage_institutions', methods=['GET'])
 def manage_institutions():
 
