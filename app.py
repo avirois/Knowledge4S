2332a7450acad4eadbc9605ea6a53c9efb0555cd
OlegBelochitsky <olegbelo8@gmail.com>
2021-03-30 11:33:16 +0300

    added route for search options

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+"""Main module from here the app starts."""
 from flask import Flask
 from blueprints.index import index_blueprint
-from blueprints.search import search_blueprint
+from blueprints.search import search_blueprint, search_options_bp
+
 
 app = Flask(__name__)
 app.register_blueprint(index_blueprint)
 app.register_blueprint(search_blueprint)
+app.register_blueprint(search_options_bp)
 app.config["DB_NAME"] = "database.db"
 app.secret_key = b"\xf5\xe4\xabr\x89\xd9#^D@0\xae[R1\xcf"
 
