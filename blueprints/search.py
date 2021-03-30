95e03e2487811d0abc5a99971cd8662dbe302162
OlegBelochitsky <olegbelo8@gmail.com>
2021-03-30 11:34:36 +0300

    added routes for base view and for selection api

--- blueprints/search.py
+++ blueprints/search.py
@@ -1,11 +1,29 @@
 """Search api module."""
-import sqlite3
-from flask import Blueprint, render_template, current_app
-
+from flask import Blueprint, render_template, request, jsonify
+from modules.selection import Selections
 
 search_blueprint = Blueprint("search_blueprint", __name__, template_folder="templates")
+search_options_bp = Blueprint(
+    "search_options_bp", __name__, template_folder="templates"
+)
+
+selections = Selections(
+    "database.db"
+)  # TODO get db fromsomewhere else maybe enviroment Variable
 
 
 @search_blueprint.route("/search", methods=["GET", "POST"])
-def route():
-    return render_template("search.html")
+def search_route():
+    selections = Selections(
+        "database.db"
+    )  # TODO get db fromsomewhere else maybe enviroment Variable
+
+    initial_selections = selections.get_selections()
+    return render_template("search.html", **initial_selections)
+
+
+@search_options_bp.route("/search/fetch/selection", methods=["POST"])
+def search_options_route():
+    current_selection = request.json
+    next_selections = selections.get_selections(**current_selection)
+    return jsonify(next_selections)
