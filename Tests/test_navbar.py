dcd1fad6d4684268dcce59d5b82e74e5ac748441
Daniel-Rabinovich <38933158+Daniel-Rabinovich@users.noreply.github.com>
2021-04-04 17:29:54 +0300

    BSPM2021T3-127 student menu UI test

--- Tests/test_navbar.py
+++ Tests/test_navbar.py
@@ -1,5 +1,20 @@
 import pytest
 from selenium import webdriver
+from flask import current_app
+import sqlite3
+
+#globals
+t_username = 'testUsername29475'
+t_password = 'kjdfvi4kjvsd4'
+t_firstname = 'john'
+t_lastname =  'doe'
+DB_NAME = 'database.db'
+
+def clean_DB():
+    connection = sqlite3.connect(DB_NAME)
+    cur = connection.execute("DELETE FROM Users WHERE UserName=(?)",(t_username,))
+    connection.commit()
+    connection.close()
 
 
 class TestNavbar:
@@ -63,3 +78,30 @@ class TestNavbar:
         elem.click()
         assert ff_browser.current_url == application + "/about"
     '''
+    #remove previous inserts
+    clean_DB()
+
+    def test_user_navbar(self, application: str, ff_browser: webdriver.Firefox):
+        connection = sqlite3.connect(DB_NAME)
+
+        #insert normal user
+        cur = connection.execute("INSERT INTO Users VALUES (?,?,?,?,?,?,?,?,?)",
+            (t_username,t_firstname,t_lastname,t_password,1,1,1,0,0,)
+        )
+
+        connection.commit()
+        connection.close()
+
+        #connect to user
+        ff_browser.get(application + "/login")
+        elem = ff_browser.find_element_by_name("username")
+        elem.send_keys(t_username)
+        elem = ff_browser.find_element_by_name("password")
+        elem.send_keys(t_password)
+        elem = ff_browser.find_element_by_name("submit")
+        elem.click()
+        elem = ff_browser.find_element_by_name("user_link")
+        assert elem.text == "My Profile"
+
+    #remove previous inserts
+    clean_DB()
