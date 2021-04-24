caa4245a42cb6ac7aaeb99bdcfb909ae759d8488
OlegBelochitsky <olegbelo8@gmail.com>
2021-04-24 19:37:49 +0300

    BSPM2021T3-204Front : write aboute the system

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ from blueprints.index import index_blueprint
 from blueprints.manageFaculty import fac_manage_blueprint
 from blueprints.manageInstitution import inst_manage_blueprint
 from blueprints.search import search_blueprint, search_options_bp
-from blueprints.uploads import upload_blueprint,UPLOAD_FOLDER
+from blueprints.uploads import upload_blueprint, UPLOAD_FOLDER
 from blueprints.manageCourse import cour_manage_blueprint
 from blueprints.view import view_blueprint
 from blueprints.serve_file import serve_blueprint
+from blueprints.about import about_bp
 
 app = Flask(__name__)
 app.register_blueprint(index_blueprint)
@@ -25,6 +26,7 @@ app.register_blueprint(fac_manage_blueprint)
 app.register_blueprint(cour_manage_blueprint)
 app.register_blueprint(view_blueprint)
 app.register_blueprint(serve_blueprint)
+app.register_blueprint(about_bp)
 app.config["DB_NAME"] = "database.db"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.secret_key = b"\xf5\xe4\xabr\x89\xd9#^D@0\xae[R1\xcf"
