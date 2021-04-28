b2a2ffa4bbec80edc09fdb12ec3e3cf47917255c
Daniel-Rabinovich <38933158+Daniel-Rabinovich@users.noreply.github.com>
2021-04-28 19:48:42 +0300

    BSPM2021T3-180 added preapproved file view in controlpanel

--- blueprints/controlpanel.py
+++ blueprints/controlpanel.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, session, redirect
-
+from flask import Blueprint, render_template, session, redirect, current_app
+import sqlite3
 
 controlpanel_blueprint = Blueprint("controlpanel_blueprint", __name__, template_folder="templates")
 
@@ -10,4 +10,23 @@ def controlpanel():
     if (session.get("admin") == None):
         return redirect('/')
 
-    return render_template("controlpanel.html")
+    # get all the preapproved files and display them
+    data = []
+    try:
+        con = sqlite3.connect(current_app.config['DB_NAME'])
+        cur = con.execute("SELECT FileID,UserName,Title,Description,DateUpload," +
+        "Institutions.InstitutionName,Faculties.FacultyName,Courses.CourseName " +
+        "FROM Files " + 
+        "NATURAL JOIN Institutions,Faculties,Courses " + 
+        "WHERE Approved = 0 " + 
+        "AND Files.InstituteID = Institutions.InstitutionID " + 
+        "AND Files.FacultyID = Faculties.FacultyID " + 
+        "AND Files.CourseID = Courses.CourseID " )
+        for row in cur:
+            data.append(row)
+    except Exception as e:
+        print(e)
+    finally:
+        con.close()
+    print(data)
+    return render_template("controlpanel.html",data = data)
