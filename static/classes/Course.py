d6623d45786acd729e44be4246dfb37b488980d9
avielro <avirois@gmail.com>
2021-05-18 11:28:47 +0300

    BSPM2021T3-212 Created backend functionality to list all messages in a course

--- static/classes/Course.py
+++ static/classes/Course.py
@@ -1,6 +1,15 @@
 import sqlite3
 from flask import current_app
+from datetime import datetime
+from static.classes.ForumMessage import ForumMessage
 
+def parse_file_time(string):
+    tmp = string.split(" ")
+    date = tmp[0]
+    time = tmp[1]
+    date = date.split("-")
+    time = time.split(":")
+    return "{}/{}/{} - {}:{}".format(date[2],date[1],date[0],time[0],time[1])
 class Course():
     """
     Class for object of Course that constructed from course id and name
@@ -64,3 +73,144 @@ class Course():
         con.close()
 
         return (self.getName())
+
+    def getSubMessagesOfMessage(self, messageID):
+        """ The function gets the sub messages of a message in a course from the DB """
+        # Connect to database and check if user exists
+        con = sqlite3.connect(current_app.config['DB_NAME'])
+
+        # Prepare the query
+        sqlQuryMessages = "SELECT UserName, Date, Message, ID FROM ForumMessages WHERE CourseID = (?) AND PreMessage = (?)"
+        # get comments
+        cur = con.execute(sqlQuryMessages, (self.getID(), messageID))
+
+        # Initialize messages list
+        messages = []
+
+        # Run over the messages of the course and append them to list
+        for row in cur:
+            #tmp = (row[0], parse_file_time(row[1]), row[2], row[3])
+            #messages.append(tmp)
+            fMessage = ForumMessage(row[3], self.getID(), parse_file_time(row[1]), row[0], row[2], messageID)
+            messages.append(fMessage)
+        
+        # Close the connection to the database
+        con.close()
+
+        return (messages)
+
+    def getMessagesOfCourse(self):
+        """ The function gets the messages of the course from the DB """
+        # Connect to database and check if user exists
+        con = sqlite3.connect(current_app.config['DB_NAME'])
+
+        # Prepare the query
+        sqlQuryMessages = "SELECT UserName, Date, Message, ID FROM ForumMessages WHERE CourseID = (?) AND PreMessage ISNULL"
+        # get comments
+        cur = con.execute(sqlQuryMessages, (self.getID(),))
+
+        # Initialize messages list
+        messages = []
+
+        # Run over the messages of the course and append them to list
+        for row in cur:
+            # Find sub messages
+            subMessages = self.getSubMessagesOfMessage(row[3])
+
+            #tmp = (row[0], parse_file_time(row[1]), row[2], row[3], subMessages)
+            #messages.append(tmp)
+            fMessage = ForumMessage(row[3], self.getID(), parse_file_time(row[1]), row[0], row[2], '-1', subMessages)
+            messages.append(fMessage)
+        
+        # Close the connection to the database
+        con.close()
+
+        return (messages)
+    
+    def getSpecificMessageForum(self, messageID):
+        """ The function gets a specific message of the course from the DB """
+        # Connect to database and check if user exists
+        con = sqlite3.connect(current_app.config['DB_NAME'])
+
+        # Prepare the query
+        sqlQuryMessages = "SELECT UserName, Date, Message, ID, CourseID, PreMessage FROM ForumMessages WHERE ID = (?)"
+        # get comments
+        cur = con.execute(sqlQuryMessages, (messageID,))
+
+        # Get the data from the result
+        row = cur.fetchone()
+
+        # Run over the messages of the course and append them to list
+        if (row):
+            #result = (row[0], parse_file_time(row[1]), row[2], row[3])
+            result = ForumMessage(row[3], row[4], parse_file_time(row[1]), row[0], row[2], row[5])
+        
+        # Close the connection to the database
+        con.close()
+
+        return (result)
+
+    def addMessageToCourse(self, message, user, preMessageID):
+        """ The function gets message and adds it to the DB """
+        # Connect to database and check if user exists
+        con = sqlite3.connect(current_app.config['DB_NAME'])
+
+        # Check if premessage exists
+        if (preMessageID == "-1"):
+            # Prepare the query
+            sqlQuryNewMessage = "INSERT into ForumMessages (CourseID, Date, UserName, Message) VALUES (?,?,?,?)"
+
+            # Execute the insert of message
+            con.execute(sqlQuryNewMessage, (self.getID(), datetime.now(), user, message,))
+        else:
+            # Prepare the query if previous message exists
+            sqlQuryNewMessage = "INSERT into ForumMessages (CourseID, Date, UserName, Message, PreMessage) VALUES (?,?,?,?,?)"
+
+            # Execute the insert of message
+            con.execute(sqlQuryNewMessage, (self.getID(), datetime.now(), user, message, preMessageID,))
+
+        # commit the changes in the DB
+        con.commit()
+        
+        # Close the connection to the database
+        con.close()
+    
+    def deleteMessageForum(self, messageID):
+        """ The function gets message ID and removes it to the DB """
+        # Connect to database and check if user exists
+        con = sqlite3.connect(current_app.config['DB_NAME'])
+
+        # Prepare query for sub messages delete
+        sqlSubMessageDel = "DELETE FROM ForumMessages WHERE PreMessage = (?)"
+
+        # Prepare the query
+        sqlQuryDelMessage = "DELETE FROM ForumMessages WHERE ID = (?)"
+        
+        # Remove sub messages
+        con.execute(sqlSubMessageDel, (messageID,))
+        
+        # Remove the message
+        con.execute(sqlQuryDelMessage, (messageID,))
+
+        # commit the changes in the DB
+        con.commit()
+        
+        # Close the connection to the database
+        con.close()
+    
+    def editMessageForum(self, messageID, newMessage):
+        """ The function gets message ID and edit the message in the DB """
+        # Connect to database and check if user exists
+        con = sqlite3.connect(current_app.config['DB_NAME'])
+
+        # Prepare the query
+        sqlQuryDelMessage = "UPDATE ForumMessages SET Message = (?) WHERE ID = (?)"
+        
+        # set the edited message
+        cur = con.execute(sqlQuryDelMessage, (newMessage, messageID,))
+
+        # commit the changes in the DB
+        con.commit()
+        
+        # Close the connection to the database
+        con.close()
